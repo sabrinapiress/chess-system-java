4b03d92dde945cf9049230cb490edb6aee502820
Sabrina C. Pires da Silva <sabrina.silva@cingo.local>
2023-02-08 11:02:18 -0300

    outras classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Board board = new Board(8, 5);
 	}
 }
